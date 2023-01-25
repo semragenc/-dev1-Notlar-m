b1879c243e578f15135c37a23ed3af773797ce8c
Semra Genc <semrademir292@gmail.com>
2023-01-26 00:21:45 +0300

    ilk deneme yapıldı.

---
+++ AkademiOdev.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("Hoş geldiniz");
+
+  var mesajSayisi = 6;
+
+  if (mesajSayisi == 0) {
+    print("Yeni mesajınız yok...");
+  } else if (mesajSayisi == 1) {
+    print("Bir yeni mesajınız var");
+  } else {
+    print("Birden fazla mesajınız var");
+  }
+}
